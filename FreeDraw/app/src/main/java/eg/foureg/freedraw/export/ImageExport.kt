c2805653a11cfa9731051142b4fc7b0321b61698
Ahmed AboElEla <eng.a.aboelela@gmail.com>
2020-05-06 11:33:14 +0200

    feature/FD_017 | Show Toast message after finishing save image

--- FreeDraw/app/src/main/java/eg/foureg/freedraw/export/ImageExport.kt
+++ FreeDraw/app/src/main/java/eg/foureg/freedraw/export/ImageExport.kt
@@ -5,12 +5,14 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.media.MediaScannerConnection
-import android.media.MediaScannerConnection.OnScanCompletedListener
 import android.os.Environment
 import android.view.View
+import android.widget.Toast
+import eg.foureg.freedraw.R
 import eg.foureg.freedraw.common.Logs
 import java.io.File
 import java.io.FileOutputStream
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -26,17 +28,23 @@ object ImageExport {
         view.draw(canvas)
 
 
-        val root = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString()
-        val myDir = File("$root/saved_images")
-        myDir.mkdirs()
+//        val root = "/Internal storage/FreeDraw"
+//        val myDir = File(root)
+//        myDir.mkdirs()
 
-        val generator = Random()
-        var n = 10000
-        n = generator.nextInt(n)
-        val fname = "Image-$n.jpg"
+//        Logs.debug(TAG, "Root Path: $root")
+//        Logs.debug(TAG, "externalMediaDirs: ${context.externalMediaDirs}")
+//        Logs.debug(TAG, "Environment.getExternalStorageState: ${Environment.getExternalStorageState()}")
+//        Logs.debug(TAG, "Environment.getRootDirectory(): ${Environment.getRootDirectory()}")
+//        Logs.debug(TAG, "context.getExternalFilesDir(Environment.DIRECTORY_DCIM): ${context.getExternalFilesDir(Environment.DIRECTORY_DCIM)}")
 
-        val file = File(myDir, fname)
 
+
+        val currentDate: String = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+        val fname = "Image-$currentDate.jpg"
+        Logs.debug(TAG, "Image Name: $fname")
+
+        val file = File(context.getExternalFilesDir("FreeDraw"), fname)
         Logs.debug(TAG, file.absolutePath)
 
         try {
@@ -45,15 +53,22 @@ object ImageExport {
             out.flush()
             out.close()
 
-            Logs.debug(TAG, "File written")
+            Logs.debug(TAG, "Exported Image File written")
 
             MediaScannerConnection.scanFile(context, arrayOf(file.toString()), null) { path, uri ->
             }
 
+            Toast.makeText(context, context.getString(R.string.txt_toast_export_image_success) + " $fname", Toast.LENGTH_LONG).show()
+
+
         } catch (e: Exception) {
             e.printStackTrace()
-            Logs.debug(TAG, "Exception in exporting image")
+            Logs.error(TAG, "Exception in exporting image")
+
+            Toast.makeText(context, context.getString(R.string.txt_toast_export_image_failure), Toast.LENGTH_LONG).show()
         }
 
     }
+
+
 }
