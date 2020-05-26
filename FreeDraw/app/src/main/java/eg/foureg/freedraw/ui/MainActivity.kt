bd1eb6cfec8590f0ac7caf578c768805f4b1474f
Ahmed AboElEla <eng.a.aboelela@gmail.com>
2020-05-26 23:16:55 +0200

    feature/FD_021 | Create drawer
    Signed-off-by: Ahmed AboElEla <eng.a.aboelela@gmail.com>

--- FreeDraw/app/src/main/java/eg/foureg/freedraw/ui/MainActivity.kt
+++ FreeDraw/app/src/main/java/eg/foureg/freedraw/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package eg.foureg.freedraw.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import eg.foureg.freedraw.R
 import eg.foureg.freedraw.common.actor.ActorMessage
 import eg.foureg.freedraw.common.actor.ActorMessageDispatcher
 import eg.foureg.freedraw.data.*
 import eg.foureg.freedraw.ui.boards.editor.BoardEditorFragment
+
 
 class MainActivity : BaseActorActivity() {
 
@@ -16,6 +21,27 @@ class MainActivity : BaseActorActivity() {
 
             navigator.navigateToBoardsListingFragment(this)
         }
+
+        // Set a Toolbar to replace the ActionBar.
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+
+        drawer =  findViewById(R.id.drawer_layout);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // The action bar home/up action should open or close the drawer.
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun updateActionBarTitle(title : String) {
@@ -46,4 +72,5 @@ class MainActivity : BaseActorActivity() {
     }
 
     val navigator : MainAcNavigator = MainAcNavigator()
+    lateinit var  drawer : DrawerLayout
 }
---
+++ FreeDraw/app/src/main/java/eg/foureg/freedraw/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,7 @@
+package eg.foureg.freedraw.ui.tools
+
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
